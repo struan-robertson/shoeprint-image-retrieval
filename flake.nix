cd8fb8502f197c77d24f1f5e1203585a0fa3ea73
Struan Robertson <contact@struanrobertson.co.uk>
2023-11-15 23:37:22 +0000

    Use nix to manage dependencies

--- flake.nix
+++ flake.nix
@@ -1,9 +1,10 @@
 {
-  description = "PyTorch Development Environment";
+  # Bit of an ugly impure solution but only PyTorch 1.13.1 works with my GPU so use pip to install it, other dependencies are installed through
+  description = "PyTorch 1.13.1 Development Environment";
 
   inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05"; };
 
-  outputs = { self, nixpkgs, nixpkgs-pytorch, ... }:
+  outputs = { self, nixpkgs, ... }:
     let
       # system should match the system you are running on
       system = "x86_64-linux";
@@ -15,14 +16,63 @@
           overlays = [ ];
         };
 
-      in pkgs.mkShell {
+      in pkgs.mkShell rec {
         # create an environment with python 3.10
 
-        packages = with pkgs; [ python310 pipenv stdenv.cc.cc.lib ];
+        name = "impurePythonEnv";
+        venvDir = "./.venv";
 
-        # shellHook = ''
-        #   exec fish
-        # '';
+        buildInputs = with pkgs;
+          [
+            # A Python interpreter including the 'venv' module is required to bootstrap
+            # the environment.
+            python310
+
+            # In this particular example, in order to compile any binary extensions they may
+            # require, the Python modules listed in the hypothetical requirements.txt need
+            # the following packages to be installed locally:
+            libdrm
+          ] ++ (with pkgs.python310Packages; [
+            # This executes some shell code to initialize a venv in $venvDir before
+            # dropping into the shell
+            venvShellHook
+
+            # Normal requirements
+            opencv4
+            keras
+            numba
+            pillow
+            numpy
+            scipy
+            tensorflow
+            tqdm
+            joblib
+            matplotlib
+
+            # Dev requirements
+            ipython
+            ipdb
+            pyqt6
+            rich
+            line_profiler
+
+          ]);
+
+        # Run this command, only after creating the virtual environment
+        postVenvCreation = ''
+          unset SOURCE_DATE_EPOCH
+          pip install -r requirements.txt
+        '';
+
+        postShellHook = ''
+          # allow pip to install wheels
+          unset SOURCE_DATE_EPOCH
+          # Fix missing libstd
+          export LD_LIBRARY_PATH=${
+            pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc ]
+          }
+        '';
+
       };
     };
 }
