dc3b32dbe6769ea8c3198cddf8b8ef4b71666218
Struan Robertson <contact@struanrobertson.co.uk>
2024-02-07 22:15:32 +0000

    Attempt at ORB similarity

--- Similarities/orb.py
+++ Similarities/orb.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import cv2
+from timm.models import convmixer
 
 def normalise_filters(print_, shoe):
     # Find global minimum and maximum across both filters
@@ -22,41 +23,63 @@ def get_similarity(print_, shoe):
 
     # detector = cv2.ORB_create() # type: ignore
     # detector = cv2.BRISK_create() # type: ignore
-    detector = cv2.AKAZE_create() # type: ignore
+    # detector = cv2.AKAZE_create() # type: ignore
+    detector = cv2.SIFT_create()
+    bf = cv2.BFMatcher()
+
+    total_similarity = 0
 
     for index in range(n_filters):
 
-
         # Print and shoe filters
-        print_filter = print_[index][1:-1, 1:-1]
-        shoe_filter = shoe[index][1:-1, 1:-1]
+        print_filter = print_[index]
+        shoe_filter = shoe[index]
 
         # print_filter, shoe_filter = normalise_filters(print_filter, shoe_filter)
         print_filter = cv2.normalize(print_filter, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U, mask=None) # type: ignore
         shoe_filter = cv2.normalize(shoe_filter, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U, mask=None) # type: ignore
 
-
         # Find the keypoints and descriptors with DETECTOR
         kp1, des1 = detector.detectAndCompute(print_filter, None)
         kp2, des2 = detector.detectAndCompute(shoe_filter, None)
 
+        if type(des1) != np.ndarray or type(des2) != np.ndarray :
+            continue
+
         # Create BFMatcher (BRUTEFORCE_HAMMING) object
-        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        # bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
         # Match descriptors
-        matches = bf.match(des1, des2)
+        # matches = bf.match(des1, des2, k=2)
+        matches = bf.knnMatch(des1, des2, k=2)
 
         # Sort them in ascending order of distance
-        matches = sorted(matches, key=lambda x: x.distance)
+        # matches = sorted(matches, key=lambda x: x.distance)
+
+        try:
+            good = []
+            for m,n in matches:
+                if m.distance < 0.75*n.distance:
+                    good.append([m])
+        except ValueError:
+            continue
+
+        # TODO currently counting the number of matches, not the _quality_
+        # import ipdb; ipdb.set_trace()
 
         # Calculate similarity score based on matches
         if len(kp1) > 0 or len(kp2) > 0:
-            similarity_score = len(matches) / (len(kp1) + len(kp2))
-            print(f"{similarity_score} {len(matches)} {len(kp1)} {len(kp2)}")
-        else:
-            similarity_score = 0
+            # total_similarity += len(matches) / (len(kp1) + len(kp2))
+            total_similarity += len(good) / (len(kp1) + len(kp2))
+        # for match in matches[:5]:
+            # total_similarity += match.distance
 
-    return 0
+
+            # print(f"{similarity_score} {len(matches)} {len(kp1)} {len(kp2)}")
+        # else:
+        #     similarity_score = 0
+
+    return total_similarity
 
 def compare_pictures(print_, shoe):
 
